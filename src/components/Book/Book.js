61d1af54517c9bbf9bd4e2f6ed2f51202fecff8e
abhichy30@gmail.com <abhichy30@gmail.com>
2021-11-26 00:41:02 +0600

    problem fixed

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -31,7 +31,7 @@ const Book = () => {
     const handleBooking = ()=>{
         console.log('Booking Clicked');
         const newBooking = {...loggedInUser, ...selectedDate};
-        fetch('http://localhost:5000/addBooking',{
+        fetch('https://vast-sands-78362.herokuapp.com/addBooking',{
             method : "POST",
             headers : {'Content-Type' : 'application/json'},
             body : JSON.stringify(newBooking)
--- src/components/Bookings/bookings.js
+++ src/components/Bookings/bookings.js
@@ -5,16 +5,24 @@ const Bookings = () => {
     const [bookings,setBookings] = useState([]);
     const [loggedInUser,setLoggedInUser] = useContext(UserContext)
     useEffect(()=>{
-        fetch(`http://localhost:5000/bookings?email=${loggedInUser.email}`,{
-            method:'POST',
-            headers:{"Content-Type":"application/json",
-            authorization:`Bearer ${sessionStorage.getItem('token')}`    
-        }
-
+        // fetch(`https://vast-sands-78362.herokuapp.com/bookings?email`+loggedInUser.email,{
+        //     method:'GET',
+        //     headers:{"Content-Type":"application/json"},
+        //     authorization:`Bearer ${sessionStorage.getItem('token')}`  ,  
+        // })
+        fetch('https://vast-sands-78362.herokuapp.com/bookings?email='+loggedInUser.email,{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                authorization:`Bearer ${sessionStorage.getItem('token')}`,
+            }
         })
         .then(res => res.json())
         .then(data => setBookings(data))
-    },[loggedInUser.email])
+    },[loggedInUser.email]);
+   
+
+    console.log(bookings);
 
     return (
         <div>
